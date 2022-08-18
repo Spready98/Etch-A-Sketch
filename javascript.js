775ee9e9e603835e01fd39bc5b3c4cf522ea7165
Trevor Situ <trevorsitu8@gmail.com>
2022-08-17 23:17:02 -0700

    added mouseover event listener for the square divs to change color and leave a trail

---
+++ javascript.js
@@ -0,0 +1,33 @@
+const container = document.querySelector("#container");
+let rows = document.getElementsByClassName("rows");
+let columns = document.getElementsByClassName("columns");
+
+function createGrid() {
+    createRows(16);
+    createColumns(16);
+}
+
+function createRows(rowNum) {
+
+    for (let i = 0; i < rowNum; i++) {
+        
+        let row = document.createElement("div");
+        row.className = "rows";
+        container.appendChild(row);
+    }
+}
+
+function createColumns(colNum) {
+
+    for (let i = 0; i < colNum; i++) {
+
+        for (let j = 0; j < colNum; j++) {
+
+            let col = document.createElement("div");
+            col.className = "columns";
+            rows[j].appendChild(col);
+        }
+    }
+}
+
+createGrid();
